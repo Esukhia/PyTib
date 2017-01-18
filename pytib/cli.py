bbba67f5b370233c8b3aae95865c044d3fa0587c
drupchen <hhdrupchen@gmail.com>
2017-01-18 01:07:54 +0100

    Raw cookiecutter template

---
+++ pytib/cli.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for pytib"""
+    click.echo("Replace this message by putting your code into "
+               "pytib.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
