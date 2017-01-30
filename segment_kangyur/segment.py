1108d95d7a25e6c842d44bb95198648bba1578ab
drupchen <hhdrupchen@gmail.com>
2017-01-30 18:04:17 +0100

    segment kangyur configuration

---
+++ segment_kangyur/segment.py
@@ -0,0 +1,14 @@
+import sys
+import os
+path = os.path.dirname(sys.modules[__name__].__file__)
+path = os.path.join(path, '..')
+sys.path.insert(0, path)
+import pytib
+from pytib.common import open_file, write_file
+
+seg = pytib.Segment()
+in_path = 'raw_kangyur'
+for f in os.listdir(in_path):
+    content = open_file('{}/{}'.format(in_path, f))
+    segmented = seg.segment()
+    write_file('output/'+f.replace('.txt', '_seg.txt'), segmented)
